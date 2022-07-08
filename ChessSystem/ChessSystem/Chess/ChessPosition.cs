507de2c8bb37c5b3a217ba725a25522ebb9f63fd
Pedrovg213 <pedrovg213@gmail.com>
2022-07-08 11:12:22 -0300

    Chess position to vector position
    - Methods and class to convert the chess position (1-8, A-H) to vector position (0-7, 0-7).

---
+++ ChessSystem/ChessSystem/Chess/ChessPosition.cs
@@ -0,0 +1,30 @@
+﻿using board;
+
+namespace Chess {
+   internal class ChessPosition {
+
+      public char Columm {
+         get; set;
+      }
+      public int Line {
+         get; set;
+      }
+
+
+      public ChessPosition( int _line , char _columm ) {
+
+         Columm = _columm;
+         Line = _line;
+      }
+
+
+      public Position ToPosition( ) {
+
+         return new Position( 8 - Line , Columm - 'A' );
+      }
+      public override string ToString( ) {
+
+         return "" + Columm + Line;
+      }
+   }
+}
--- ChessSystem/ChessSystem/Program.cs
+++ ChessSystem/ChessSystem/Program.cs
@@ -6,17 +6,10 @@ namespace ChessSystem {
    internal class Program {
       static void Main( string[ ] args ) {
 
-         try {
-            Board board = new Board(8, 8);
+         ChessPosition chessPosition = new ChessPosition(7, 'C');
 
-            board.PutPieces( new Castle( board , Color.Black ) , new Position( 0 , 0 ) );
-            board.PutPieces( new Castle( board , Color.Black ) , new Position( 1 , 3 ) );
-            board.PutPieces( new King( board , Color.White ) , new Position( 5 , 0 ) );
-
-            Screen.PrintBoard( board );
-         } catch ( BoardException be ) {
-            Console.WriteLine( be.Message );
-         }
+         Console.WriteLine( chessPosition );
+         Console.WriteLine( chessPosition.ToPosition() );
       }
    }
 }
